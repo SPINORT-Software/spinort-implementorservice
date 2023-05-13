76d661a9bebf3ae3059900c2a6806d5c2355e3aa
Sharan Chitta <chitta.sharan@gmail.com>
2023-05-13 16:36:45 -0400

    Update test gpio

--- test_gpio.py
+++ test_gpio.py
@@ -10,7 +10,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(18, GPIO.OUT)
 
-p = GPIO.PWM(18, 150)  # channel=12 frequency=50Hz
+p = GPIO.PWM(18, 1000)  # channel=12 frequency=50Hz
 p.start(0)
 try:
     while 1:
