a9bd0dfb082f207257bdaa0e5df10916f1aa0b90
Sharan Chitta <chitta.sharan@gmail.com>
2023-05-13 16:08:49 -0400

    GPIO alternative code

--- test_gpio.py
+++ test_gpio.py
@@ -1,5 +1,26 @@
-from gpio_apply import GpioApply
+# from gpio_apply import GpioApply
+#
+# gpio = GpioApply()
+#
+# gpio.apply_pwm(100)
 
-gpio = GpioApply()
 
-gpio.apply_pwm(100)
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(18, GPIO.OUT)
+
+p = GPIO.PWM(18, 50)  # channel=12 frequency=50Hz
+p.start(0)
+try:
+    while 1:
+        for dc in range(0, 101, 5):
+            p.ChangeDutyCycle(dc)
+            time.sleep(0.1)
+        for dc in range(100, -1, -5):
+            p.ChangeDutyCycle(dc)
+            time.sleep(0.1)
+except KeyboardInterrupt:
+    pass
+p.stop()
+GPIO.cleanup()
