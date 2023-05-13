63c64959e11035eb2b73c9ca611eb1e22f7a6c0c
Sharan Chitta <chitta.sharan@gmail.com>
2023-05-13 15:19:25 -0400

    remove confluent kafka consumer

--- consumer.py
+++ consumer.py
@@ -1,8 +1,18 @@
+# When the UI triggers a start process - generate a UNIQUE ID for the user
+# send this key always in the KAFKA messages from that user for that session
+# the UNIQUE ID can be called SESSION_ID in the kafka message
+# SESSION_ID is mapped to a USER in the django db.
+
+
+# Consumer thread runs continously to read the messages from topic and send to assembler
+# From the assembler - return a sensorData POJO class object
+#
 import logging
 import threading
 from abc import ABCMeta, abstractmethod
 
-from confluent_kafka import Consumer, OFFSET_BEGINNING, KafkaException, KafkaError
+from kafka import KafkaConsumer
+from infra.assemblers.error_handler import KafkaErrorHandler
 
 logger = logging.getLogger(__name__)
 _KAFKA_MAX_INT_VALUE = 2147483647
@@ -22,21 +32,25 @@ class KafkaConsumerConfiguration(metaclass=ABCMeta):
         pass
 
 
-class KafkaConsumer(threading.Thread):
+class Consumer(threading.Thread):
     def __init__(
             self,
-            kafka_consumer_configuration,
-            env_config,
+            kafka_consumer_configuration: KafkaConsumerConfiguration,
             topic: str,
+            error_handler: KafkaErrorHandler,
             callback_function=lambda event: print(event),
             **kwargs
     ):
         threading.Thread.__init__(self, name=f"kafkaconsumer_[{topic}]")
         self._stop_event = threading.Event()
+        self._boostrap_servers = kafka_consumer_configuration.get_bootstrap_servers()
+        self._sasl_plain_username = kafka_consumer_configuration.get_sasl_plain_username()
+        self._sasl_plain_password = kafka_consumer_configuration.get_sasl_plain_password()
         self._topic = topic
+        self._group_id = kafka_consumer_configuration.get_consumer_group_id()
+        self._consumer_timeout_ms = kafka_consumer_configuration.get_consumer_timeout_ms()
+        self._error_handler = error_handler
         self._callback_function = callback_function
-        self._kafka_consumer_configuration = kafka_consumer_configuration
-        self._env_config = env_config
         if kwargs:
             self._kwargs = kwargs
 
@@ -45,33 +59,37 @@ class KafkaConsumer(threading.Thread):
 
     def run(self):
         try:
-            group_id = self._env_config.get_kafka_consumer_configuration().get_consumer_group_id()
+            consumer = KafkaConsumer(
+                bootstrap_servers=self._boostrap_servers,
+                sasl_mechanism="PLAIN",
+                security_protocol="SASL_SSL",
+                sasl_plain_username=self._sasl_plain_username,
+                sasl_plain_password=self._sasl_plain_password,
+                auto_offset_reset="latest",  # earliest to get all the messages in the queue
+                api_version=(0, 10, 1),
+                group_id=self._group_id,
+                # group_id=None,
+                consumer_timeout_ms=self._consumer_timeout_ms,
+                max_poll_interval_ms=_KAFKA_MAX_INT_VALUE,
+                **self._kwargs
+            )
 
-            self._kafka_consumer_configuration["group.id"] = group_id
-            self._kafka_consumer_configuration["auto.offset.reset"] = "latest"
-            consumer = Consumer(self._kafka_consumer_configuration)
             consumer.subscribe([self._topic])
-            logger.info(f"Subscribing to topic: [{self._topic}] with consumer group.id: {group_id}")
+            logger.info(f"Subscribing to topic: [{self._topic}] at {self._boostrap_servers}")
 
             while not self._stop_event.is_set():
-                try:
-                    message = consumer.poll()
-                    if message is None: continue
-                    if message.error():
-                        if message.error().code() == KafkaError._PARTITION_EOF:
-                            # End of partition event
-                            logger.exception('%% %s [%d] reached end at offset %d\n' % (
-                                message.topic(), message.partition(), message.offset()))
-                        elif message.error():
-                            raise KafkaException(message.error())
-                    else:
-                        logger.info(f"Consuming {message.topic()}-{message.partition()}-{message.offset()}")
-                        consumer.commit(asynchronous=True)
+                for message in consumer:
+                    try:
+                        logger.info(f"Consuming {message.topic}-{message.partition}-{message.offset}")
+
+                        """
+                        Get the device_id from kafka message and store payload to the models.
+                        """
                         self._callback_function(message)
-                except Exception as exception:
-                    logger.exception(f"The kafka event could not be consumed {exception}")
-                if self._stop_event.is_set():
-                    break
+                    except Exception as exception:
+                        logger.exception(f"The kafka event could not be consumed {exception}")
+                    if self._stop_event.is_set():
+                        break
 
             logger.info(f"Stop event received for consumer of topic [{self._topic}]. Consumption will be stopped")
             consumer.close()
