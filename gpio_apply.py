499ea03ce533a86704556fca57c5c83d76a58e8a
Sharan Chitta <chitta.sharan@gmail.com>
2023-06-26 18:01:23 -0400

    Add two Channels

--- gpio_apply.py
+++ gpio_apply.py
@@ -4,17 +4,24 @@ import RPi.GPIO as GPIO
 def apply_pwm(value):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(32, GPIO.OUT)
-    p = GPIO.PWM(32, 1000)
-    p.start(0)
+    p1 = GPIO.PWM(32, 1000)
+
+    GPIO.setup(12, GPIO.OUT)
+    p2 = GPIO.PWM(12, 1000)
+
+    p1.start(0)
+    p2.start(0)
     try:
         for dc in range(0, value + 1, 5):
             print(f"Setting the GPIO brightness to {dc}")
-            p.ChangeDutyCycle(dc)
+            p1.ChangeDutyCycle(dc)
+            p2.ChangeDutyCycle(dc)
             time.sleep(2)
     except KeyboardInterrupt:
         pass
     print(f"Stopping the PWM.")
-    p.stop()
+    p1.stop()
+    p2.stop()
     GPIO.cleanup()
 
 
