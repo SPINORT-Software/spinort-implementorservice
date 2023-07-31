369b6636c75564fd8af9d5e7dc367b714f612f33
Sharan Chitta <chitta.sharan@gmail.com>
2023-07-31 17:35:54 -0400

    Convert engine result value to duty cycle

--- gpio_apply.py
+++ gpio_apply.py
@@ -1,19 +1,38 @@
 import time
 import RPi.GPIO as GPIO
 
+
+def convert_to_duty_cycle(value):
+    """
+    Condition – 1: If the engine calculated Energy Output Value &lt;/= 15,
+        Then duty cycle = [{5 / (Energy Output Value + 15)} x 100]
+    Condition – 2: If the engine calculated Energy Output Value &gt; 15,
+        Then duty cycle = {(5 / Energy Output Value) x 100}
+    :param value:
+    :return:
+    """
+    if value <= 15:
+        return (5 / (value + 15)) * 100
+    else:
+        return (5 / value) * 100
+
+
 def apply_pwm(value, side):
     print(f"Received stimulation value of {value} to apply on side: {side}")
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(32, GPIO.OUT)
-    p1 = GPIO.PWM(32, 20) # left
+    p1 = GPIO.PWM(32, 20)  # left
 
     GPIO.setup(12, GPIO.OUT)
-    p2 = GPIO.PWM(12, 20) # right
+    p2 = GPIO.PWM(12, 20)  # right
 
     p1.start(0)
     p2.start(0)
+
+    duty_cycle_value = convert_to_duty_cycle(value)
+
     try:
-        for dc in range(0, value + 1, 5):
+        for dc in range(0, duty_cycle_value + 1, 5):
             print(f"Setting the GPIO brightness to {dc}")
             if side == 'right':
                 p2.ChangeDutyCycle(dc)
@@ -34,6 +53,3 @@ def apply_pwm(value, side):
     p1.stop()
     p2.stop()
     GPIO.cleanup()
-
-
-
